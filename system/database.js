eaba228743de596615233150b13faf11281f8d73
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-09 23:08:26 +0100

    Update database.js

--- system/database.js
+++ system/database.js
@@ -89,6 +89,8 @@ module.exports = (m) => {
       global.db.setting = {
          autodownload: true,
          chatbot: true,
+         sk_pack: 'MOHAMED BELLA ❤️',
+         sk_author: '@wox_bella',
          debug: false,
          error: [],
          pluginDisable: [],
@@ -99,8 +101,8 @@ module.exports = (m) => {
          toxic: ["ajg", "ajig", "anjas", "anjg", "anjim", "anjing", "anjrot", "anying", "asw", "autis", "babi", "bacod", "bacot", "bagong", "bajingan", "bangsad", "bangsat", "bastard", "bego", "bgsd", "biadab", "biadap", "bitch", "bngst", "bodoh", "bokep", "cocote", "coli", "colmek", "comli", "dajjal", "dancok", "dongo", "fuck", "gelay", "goblog", "goblok", "guoblog", "guoblok", "hairul", "henceut", "idiot", "itil", "jamet", "jancok", "jembut", "jingan", "kafir", "kanjut", "kanyut", "keparat", "kntl", "kontol", "lana", "loli", "lont", "lonte", "mancing", "meki", "memek", "ngentod", "ngentot", "ngewe", "ngocok", "ngtd", "njeng", "njing", "njinx", "oppai", "pantek", "pantek", "peler", "pepek", "pilat", "pler", "pornhub", "pucek", "puki", "pukimak", "redhub", "sange", "setan", "silit", "telaso", "tempek", "tete", "titit", "toket", "tolol", "tomlol", "tytyd", "wildan", "xnxx"],
          online: true,
          onlyprefix: '+',
-         owners: ['6285887776722', '994408364923'],
-         msg: 'I am a Whatsapp BOT, use a bot in private chat so that bots can respond quickly.'
+         owners: ['212632244668'],
+         msg: 'hello my name is Mohamed bella'
       }
    }
 }
