084044de104720c7bbf61b8a7f975d02b79f2cb7
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-10 19:16:27 +0100

    Update limit.js

--- plugins/userinfo/limit.js
+++ plugins/userinfo/limit.js
@@ -5,7 +5,7 @@ exports.run = {
       isPrefix,
    }) => {
       let user = global.db.users[m.sender]
-      if (user.limit < 1) return client.reply(m.chat, `🚩 Your bot usage has reached the limit and will be reset at 00.00\n\nTo get more limits, upgrade to a premium plan send *${isPrefix}premium*`, m)
+      if (user.limit < 1) return client.reply(m.chat, `🚩 لقد وصلت للعدد الاقصى من الرسائل التي يمكنكم إرسالها لي ، يمكنكم إعادة إستعمال البوت بعد 24 ساعة*`, m)
       client.reply(m.chat, `🍟 Your limit : [ *${Func.formatNumber(user.limit)}* ]${!user.premium ? `\n\nTo get more limits, upgrade to a premium plan send *${isPrefix}premium*` : ''}`, m)
    },
    error: false
