182d75a294ad342a52b0501a78bfe0b6358b2078
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-10 03:41:01 +0100

    Update maths.js

--- plugins/bac/maths.js
+++ plugins/bac/maths.js
@@ -1,1 +1,31 @@
 
+exports.run = {
+   usage: ['maths'],
+   async: async (m, {
+      client,
+      isPrefix
+   }) => {
+      let rows = [
+         {
+         title: 'LECON 1',
+         rowId: `${isPrefix}mediafire http://www.mediafire.com/file/4hq1nadce9yq6lz/examens_bac_PC_SC.PHYS_2008_-_2016_avec_corr.rar`,
+         description: ``
+         },
+
+         {
+         title: 'lecon 2',
+         rowId: `${isPrefix}mediafire http://www.mediafire.com/file/4hq1nadce9yq6lz/examens_bac_PC_SC.PHYS_2008_-_2016_avec_corr.rar`,
+         description: ``
+         }
+
+]
+      let text = 'MATHEMATIQUES ❤️😇\n\n'
+      text += '◦ *💟DEVELOPER* : *MOHAMED BELLA*\n'
+      text += '◦ *💟INSTAGRAM* : https://instagram.com/wox_bella *(v2.2.0)*\n\n'
+      text += 'If you find an error or want to upgrade premium plan contact the owner.'
+      await client.sendList(m.chat, '', text, '', 'إضغط هنا!', [{
+         rows
+      }], m)
+   },
+   error: false
+}
