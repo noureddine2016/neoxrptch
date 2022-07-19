6653c25465652b02dcd84d390baaf98a5e1cc57c
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-19 17:21:15 +0100

    Update phylo.js

--- plugins/bac/phylo.js
+++ plugins/bac/phylo.js
@@ -7,16 +7,102 @@ exports.run = {
    }) => {
       let rows = [
          {
-         title: 'LEÇON 1',
-         rowId: `${isPrefix}mediafire https://www.mediafire.com/file/4hq1nadce9yq6lz/examens_bac_PC_SC.PHYS_2008_-_2016_avec_corr.rar`,
+         title: 'مجزوءة الوضع البشري - الشخص بين الضرورة والحرية - مراجعة وشرح بالامتلة + 100% فهم | الحصة رقم 3',
+         rowId: `https://www.youtube.com/watch?v=d5ORIVaY9A8&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'مجزوءة الوضع البشري - الشخص والهوية - مراجعة وشرح بالامتلة + 100% فهم | الحصة رقم 1',
+         rowId: `https://www.youtube.com/watch?v=77YH8aqBCRE&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'مجزوءة الوضع البشري - الشخص بوصفه قيمة - مراجعة وشرح بالامتلة + 100% فهم | الحصة رقم 2',
+         rowId: `https://www.youtube.com/watch?v=zb1XPmueFgM&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'قيمة الاطروحة وحدودها - كيفاش تضمن 3 نقاط فالوطني !!',
+         rowId: `https://www.youtube.com/watch?v=bszcBUhOhHE&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'مجزوءة الوضع البشري - وجود الغير - مراجعة وشرح بالامتلة + 100% فهم | الحصة رقم 4',
+         rowId:` https://www.youtube.com/watch?v=eOyhLvIAyQE&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'مجزوءة الوضع البشري - معرفة الغير - مراجعة وشرح بالامتلة + 100% فهم | الحصة رقم 5',
+         rowId: `https://www.youtube.com/watch?v=dmQgl277bWI&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'مجزوءة الوضع البشري - العلاقة مع الغير - مراجعة وشرح بالامتلة + 100% فهم | الحصة رقم 6',
+         rowId: `https://www.youtube.com/watch?v=KU6rW8eAMts&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'مفهوم التاريخ - المعرفة التاريخية - شرح بالامثلة من الواقع 100% فهم ( الوضع البشري )',
+         rowId: `https://www.youtube.com/watch?v=x2ybZYHkjBI&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'احسن ملخص لدرس الدولة مجزوءة السياسة / الدقيقة 90',
+         rowId: `https://www.youtube.com/watch?v=QqEHnB6O9io&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'منهجية النص - منهجية القولة - منهجية السؤال / افهم وطبق 18/20 مضمونة',
+         rowId: `https://www.youtube.com/watch?v=9bY7XcB_kw4&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : 'احسن ملخص لدرس الغير مجزوءة الوضع البشري / الدقيقة 90',
+         rowId: `https://www.youtube.com/watch?v=0SeudX37-Wg&ab_channel=%D9%85%D9%88%D9%84%D8%A7%D9%84%D9%81%D9%84%D8%B3%D9%81%D8%A9molfalsafa`,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
+         description: `insta : @wox_bella`
+         },
+          {
+         title : '',
+         rowId: ``,
          description: `insta : @wox_bella`
          },
 
-         {
-         title: 'LEÇON 2',
-         rowId: `${isPrefix}mediafire https://www.mediafire.com/file/4hq1nadce9yq6lz/examens_bac_PC_SC.PHYS_2008_-_2016_avec_corr.rar`,
-         description: `insta : @wox_bella`
-         }
+  
 
 ]
       let text = '💟 *PHYLOSOPHIE* ❤️😇\n\n'
