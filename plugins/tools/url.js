c6920a38fbe3bd66d8bcb94198a4de0cc58609f7
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-20 18:51:49 +0100

    Update url.js

--- plugins/tools/url.js
+++ plugins/tools/url.js
@@ -8,19 +8,6 @@ exports.run = {
    }) => {
       try {
          
-         
-         if (command == 'short') {
-            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, command != 'artinama' ? 'yntkts' : 'wildan'), m)
-            let json = await scrap.mo3jam(text)
-            command != 'artinama' ? client.reply(m.chat, `${json.data.content}`, m) : client.reply(m.chat, `${json.data.content}`, m)
-         }
-         
-          if (command == 'short') {
-            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, command != 'artinama' ? 'yntkts' : 'wildan'), m)
-            let json = await scrap.mo3jam(text)
-            command != 'artinama' ? client.reply(m.chat, `${json.data.content}`, m) : client.reply(m.chat, `${json.data.content}`, m)
-         }
-         
           if (command == 'mo3jam') {
             if (!text) return client.reply(m.chat, Func.example(isPrefix, command, command != 'artinama' ? 'yntkts' : 'wildan'), m)
             let json = await scrap.mo3jam(text)
