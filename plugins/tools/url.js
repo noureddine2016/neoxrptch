4f7a22fbd6941cc24900078d02bd3848f8a9b3d1
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-20 18:51:09 +0100

    Update url.js

--- plugins/tools/url.js
+++ plugins/tools/url.js
@@ -1,5 +1,5 @@
 exports.run = {
-   usage: ['mo3jam'],
+   usage: ['mo3jam', 'mo3jam' , 'mo3jam' ]
    async: async (m, {
       client,
       text,
@@ -7,12 +7,40 @@ exports.run = {
       command
    }) => {
       try {
-         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, command != 'artinama' ? 'yntkts' : 'wildan'), m)
-         let json = await scrap.mo3jam(text)
-         command != 'artinama' ? client.reply(m.chat, `${json.data.content}`, m) : client.reply(m.chat, `${json.data.content}`, m)
+         
+         
+         if (command == 'short') {
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, command != 'artinama' ? 'yntkts' : 'wildan'), m)
+            let json = await scrap.mo3jam(text)
+            command != 'artinama' ? client.reply(m.chat, `${json.data.content}`, m) : client.reply(m.chat, `${json.data.content}`, m)
+         }
+         
+          if (command == 'short') {
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, command != 'artinama' ? 'yntkts' : 'wildan'), m)
+            let json = await scrap.mo3jam(text)
+            command != 'artinama' ? client.reply(m.chat, `${json.data.content}`, m) : client.reply(m.chat, `${json.data.content}`, m)
+         }
+         
+          if (command == 'mo3jam') {
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, command != 'artinama' ? 'yntkts' : 'wildan'), m)
+            let json = await scrap.mo3jam(text)
+            command != 'artinama' ? client.reply(m.chat, `${json.data.content}`, m) : client.reply(m.chat, `${json.data.content}`, m)
+         }
+         
+         
+         
+         
+         
       } catch {
          client.reply(m.chat, global.status.error, m)
       }
    },
    error: false
+   
+   
+   
+   
+   
+   
+   
 }
