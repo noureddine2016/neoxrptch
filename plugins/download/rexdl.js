710f918e6e6c79ab8d062bfc8573b210fc1fed9a
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-21 14:41:17 +0100

    Update rexdl.js

--- plugins/download/rexdl.js
+++ plugins/download/rexdl.js
@@ -10,10 +10,10 @@ exports.run = {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://www.mediafire.com/file/1fqjqg7e8e2v3ao/YOWA.v8.87_By.SamMods.apk/file'), m)
          client.sendReact(m.chat, '🕒', m.key)
-         let json = await Api.rexdl(args[0])
+         let json = await Api.rexdl(args)
          if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
          let text = `乂  *R E X D L*\n\n`
-         text += '	◦  *Name* : ' + unescape(decode(json.data.name)) + '\n'
+         text += '	◦  *Name* : ' + json.data.name + '\n'
          text += '	◦  *category* : ' + json.data.category + '\n'
          text += '	◦  *publish* : ' + json.data.publish + '\n'
          text += '	◦  *desc* : ' + json.data.desc + '\n'
